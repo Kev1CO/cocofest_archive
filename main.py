dfb01a452a2da3d142cdf0dfa583231549e2790e
Kevin CO <kevinco@umontreal.ca>
2022-08-19 11:26:27 -0400

    Update of the main model
    Force model

--- main.py
+++ main.py
@@ -1,85 +1,116 @@
-# This is a sample Python script.
+import numpy as np # Import numpy for mathematical purpose
+from matplotlib import pyplot # Import matplotlib for graphics
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+##### Force Model #####
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import numpy as np
+# Real values / Known values :
+Tauc = 0.020  # (s) time constant controlling the rise and decay of CN for quadriceps
+R0 = 2  # (-) mathematical term characterizing the magnitude of enhancement inCNfrom the following stimuli. When fatigue included : R0 = Km + 1.04
+n = 2  # (-) total number of stimulus in the train before time t (single, doublets, triplets)
+
+# Arbitrary values / Different for each person :
+A = 3.009  # (N/s) scaling factor for the force and the shortening velocity of the muscle
+Tau1 = 0.050957  # (s) time constant of force decline at the absence of strongly bound cross-bridges
+Tau2 = 0.015  # (s) time constant of force decline due to the extra friction between actin and myosin resulting from the presence of cross-bridges
+Km = 0.103  # (-) sensitivity of strongly bound cross-bridges to CN
+CN = 2  # (-) representation of Ca2+-troponin complex
+F = 40  # (N) instantaneous force
+
+# Stimulation parameters :
+ti = 0.0005  # (s) time of the ith stimulation
+tp = 0.001  # (s) time of the pth data point
+u = [0., 0.1 , 1.1] # Electrical stimulation activation time
+# ti_all = np.array([0., 0.0005]) not implemented (need to be implemented with a ti_list for each activation)
+
+# Simulation parameters :
+final_time = 2  # Stop at x seconds
+dt = 0.00001 # Integration step
+x_initial = np.array([0,0]) # Initial CN and F
+u_instant = 0 # Used in x_dot function
+
+# Euler integration method
+def euler(dt, x, dot_fun, u, t):
+    return x + dot_fun(x, u, t) * dt
+
+# x_dot function
+def x_dot(x, u, t):
+    # Initialization
+    CN = x[0]
+    F = x[1]
+    var_sum = 0
+    global u_instant
+
+    # See if t equals an activation time u (round up to prevent flot issues)
+    if round(t, 5) in u:
+        u_instant = t
+
+    # Variables calculation for equation 1 of the force model
+    Ri = 1 + (R0 - 1) * np.exp(-((ti-ti) / Tauc))
+    var_sum += Ri * np.exp(-(t - (ti + u_instant)) / Tauc)
+
+    # Remove activation at t = 0 if not requested
+    if t < min(u):
+        var_sum = 0
+
+    ## Two idea to keep ##
+
+    # for ti in range(1, len(ti_all)):
+    #     Ri = 1 + (R0 - 1) * np.exp(-((ti_all[ti]-ti_all[ti-1]) / Tauc))
+    #     var_sum += Ri * np.exp(-(t - ti_all[ti]) / Tauc)
+    # for ti in range(0, n):
+    #     Ri = 1 + (R0 - 1) * np.exp(-((ti_all[ti]-ti_all[ti-1]) / Tauc))
+    #     var_sum += Ri * np.exp(-(t - ti_all[ti]) / Tauc)
 
 
-class Ding_model(object):
+    CNdot = (1 / Tauc) * var_sum - CN / Tauc # Eq(1)
+    Fdot = A * (CN / (Km + CN)) - (F / (Tau1 + Tau2 * (CN / (Km + CN)))) # Eq(2)
 
-    def __int__(self):
+    CNdot = np.array([CNdot]) # Put in array type
+    Fdot = np.array([Fdot]) # Put in array type
 
-        ##### Force Model #####
-        # Valeurs réels :
-        Tc = 20 #constante de temps controllant l'augmentation et la dégression de CN (ms)
-        R0 = 2 #terme matématique caractérisant la grandeur d'enchainement du CN (sans unitée). Peux se calculer plus précisément R0 = Km + 1.04
-
-        #Valeurs arbitraires :
-        A = 2 # (N/ms) scaling factor for the force and the shortening velocity of the muscle
-        T1 = 10 # (ms) time constant of force decline at the absence of strongly bound cross-bridges
-        T2 = 15 # (ms) time constant of force decline due to the extra friction between actin and myosin resulting from the presence of cross-bridges
-        Km = 4 # (-) sensitivity of strongly bound cross-bridges to CN
-        CN = 2 # (-) representation of Ca2+-troponin complex
-        F = 40 # (N) instantaneous force
-        h = 0.1 # (s) integration step
-        ti = 1 # (ms) time of the ith stimulation
-        tp = 1 # (ms) time of the pth data point
+    return np.concatenate((CNdot, Fdot),axis=0)
 
 
-    ##### Force Model #####
+# Now assuming some initial values, we can perform the integration up to a required final time
+def perform_integration(final_time, dt, x_initial, x_dot_fun, u, integration_fun):
+    time_vector = [0.]
+    all_x = [x_initial]
+    while time_vector[-1] <= final_time:  # As long as we did not get to the final time continue
+        time_vector.append(time_vector[-1] + dt)  # The next time is dt later
+        all_x.append(integration_fun(dt, x_initial, x_dot_fun, u, time_vector[-1]))  # Integrate
+        x_initial = all_x[-1]  # The next x is the arrival state of the previous integration
 
-    def Ri(self, dt, time, beginning=0, end=1):
-        Ri = 1+(R0-1)*np.exp(-(time-(time-dt)/Tc))
-        return Ri
+    # Format the time vector and the x so they are easier to use
+    time_vector = np.array(time_vector)
+    all_x = np.array(all_x).transpose()
 
-    def T1(self, t, F, F0): #déterminer t, comprendre pourquoi une division proche de +infini
-        T1 = (-t)/np.log(F/F0) #By taking the force decay at the end of the tetany and performing a linear regression of Ln(F)
-        # versus t via the SAAM II numerical module, we obtained the value T1 from the slope
-        return T1
+    return time_vector, all_x
 
-    def force_model(self, Tc, Ri): #Ri array_type/list_type
-        dotCN = (1/Tc)*np.sum(Ri*np.exp(-(t-ti)/Tc)-CN/Tc) #Eq(1)
-        dotF = A*(Cn/(Km+CN))-(F/(T1+T2*(CN/(Km+CN)))) #Eq(2)
-        return dotCN, dotF
+# Figure out when electrical stimulation is activated
+def stim_signal(ti , u):
+    time_vector = [0.]
+    stim_signal_y = [0]
+    while time_vector[-1] <= final_time: # As long as we did not get to the final time continue
+        time_vector.append(time_vector[-1] + dt) # The next time is dt later
+        if any(round(time_vector[-1], 5) >= i and round(time_vector[-1], 5) <= i + ti for i in u) : # See if an activation belongs to t
+            stim_signal_y.append(1) # Yes
+        else:
+            stim_signal_y.append(0) # No
 
-    def Gnf(self, Fpred, Fexp): #fonction à minimiser par Sequential Quadratic Programing (SQP)
-        Gnf = np.sum(Fpred-Fexp)**2 #Eq(3)
-        return Gnf, T2
+    return stim_signal_y
 
-    def Gfat(self, Fpred, Fexp): #fonction à minimiser par Sequential Quadratic Programing (SQP)
-        Gfat = np.sum(Fpred-Fexp)**2 #Eq(4)
-        return Gfat, A, Km
 
-    ##### Fatigue Model #####
 
-    def dotA(self, A, Arest, Tfat, AlphaA, F):
-        dotA = -((A-Arest)/Tfat)+AlphaA*F #Eq(5)
-        return dotA
+time_vector_euler, all_x_euler = perform_integration(final_time, dt, x_initial, x_dot, u, euler)
+stim_signal_y = stim_signal(ti , u)
 
-    def Km(self, Km1, Km2):
-        Km = Km1+Km2 #Eq(6)
-        return Km
-
-    def dotKm1(self, Km1, Km1rest, Tkm, AlphaKm, F):
-        dotKm1 = -((Km1-Km1rest)/Tkm)-AlphaKm*F#Eq(7)
-        return dotKm1
-
-    def dotKm2(self, Km2, Km2rest, Tkm, AlphaKm, F):
-        dotKm2 = -((Km2-Km2rest)/Tkm)-AlphaKm*F #Eq(8)
-        return dotKm2
-
-    def dotT1(self, T1, T1rest, Tfat, AlphaT1, F):
-        dotT1 = -((T1-T1rest)/Tfat)+AlphaT1*F #Eq(9)
-        return dotT1
-
-    def G(self, Ppred, Pcalc): #fonction à minimiser par Sequential Quadratic Programing (SQP)
-        G = np.sum(np.sum([1-(Ppred/Pcalc)]**2)) #Eq(10)
-        return G
-
-    def dotKm(self, Km, Kmrest, Tfat, AlphaKm, F):
-        dotKm = -((Km-Kmrest)/Tfat)+AlphaKm*F #Eq(11)
-        return dotKm
-
+# We can now compare plot the two functions on the same graph
+pyplot.figure()
+pyplot.plot(time_vector_euler, all_x_euler[0, :], 'blue', label='CN (-)') # Function of the Ca2+-troponin complex
+pyplot.plot(time_vector_euler, all_x_euler[1, :], 'green', label='F (N)') # Function of the force
+pyplot.plot(time_vector_euler, stim_signal_y[:], 'r-', label='Stim') # Function of electrical stimulation activation
+pyplot.legend()
+pyplot.ylabel("Force (N)")
+pyplot.xlabel("Time (s)")
+pyplot.show()
 
