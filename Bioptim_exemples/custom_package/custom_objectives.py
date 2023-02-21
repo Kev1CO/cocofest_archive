35d4525f1b0651db770e93e6d77dac7a748a750d
Kevin CO <kevinco@umontreal.ca>
2023-02-21 18:28:02 -0500

    Custom force objective

--- Bioptim_exemples/custom_package/custom_objectives.py
+++ Bioptim_exemples/custom_package/custom_objectives.py
@@ -1,33 +1,23 @@
 from bioptim import PenaltyNodeList
 from bioptim.interfaces.biorbd_model import BiorbdModel
+from casadi import MX
 
 
-def track_muscle_force_custom(all_pn: PenaltyNodeList, force: int | float):
+def track_muscle_force_custom(all_pn: PenaltyNodeList | list, force: int | float) -> MX:
     """
-    Minimize the difference of the model muscle force produiced and the targeted muscle force
+    Minimize the difference between the force produced by the model and the targeted force
     By default this function is quadratic, meaning that it minimizes the difference.
 
     Parameters
     ----------
     all_pn: PenaltyNodeList
         The penalty node elements
-    force: Union[int, str]
+    force: int | str
         The name or index of the segment
     """
 
-    penalty.quadratic = True if penalty.quadratic is None else penalty.quadratic
-
     nlp = all_pn.nlp
-    force_index = nlp.model.segment_index(force) if isinstance(force, str) else force
-
-    if not isinstance(nlp.model, BiorbdModel):
-        raise NotImplementedError("The track_muscle_force_custom penalty can only be called with a BiorbdModel")
-    model: BiorbdModel = nlp.model
-
-    current_force = nlp.model  # Comment recuperer la force ?
-    # DingModel.system_dynamics[1] ?
-
+    current_force = nlp.states[0].cx
     force_diff = (current_force - force) ** 2
 
-    force_objective = nlp.mx_to_cx(f"track_force", force_diff, nlp.states["q"])  # nlp.states["q"] ??
-    return force_objective
+    return force_diff
