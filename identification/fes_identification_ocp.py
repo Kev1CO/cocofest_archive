New average identification (still wrong behavior on discontinuity)

identification/fes_identification_ocp.py
@@ -73,7 +73,6 @@ class FunctionalElectricStimulationOptimalControlProgramIdentification(OptimalCo
         ding_model: DingModelFrequency | DingModelPulseDurationFrequency | DingModelIntensityFrequency = None,
         with_fatigue: bool = None,
         stimulated_n_shooting: int = None,
-        rest_n_shooting: int = None,
         force_tracking: list[np.ndarray, np.ndarray] = None,
         pulse_apparition_time: list[int] | list[float] = None,
         pulse_duration: list[int] | list[float] = None,
@@ -107,11 +106,6 @@ class FunctionalElectricStimulationOptimalControlProgramIdentification(OptimalCo
             raise TypeError(
                 f"force_tracking must be list type," f" currently force_tracking is {type(force_tracking)}) type."
             )
-        self.force_tracking = force_tracking
-        self.discontinuity_in_ocp = discontinuity_in_ocp
-
-        self.parameter_mappings = None
-        self.parameters = None
 
         if not isinstance(pulse_apparition_time, list):
             raise TypeError(
@@ -132,15 +126,19 @@ class FunctionalElectricStimulationOptimalControlProgramIdentification(OptimalCo
                     f" currently pulse_intensity is {type(pulse_intensity)}) type."
                 )
 
+        self.force_tracking = force_tracking
+        self.discontinuity_in_ocp = discontinuity_in_ocp
+
+        self.parameter_mappings = None
+        self.parameters = None
+
         self.ding_models = [self.ding_model for i in range(len(pulse_apparition_time))]
 
         for i in range(len(pulse_apparition_time)):
             self.final_time_phase = (
-                (round(pulse_apparition_time[i + 1], 4),)
+                (pulse_apparition_time[i + 1],)
                 if i == 0
-                else self.final_time_phase + (round(pulse_apparition_time[i] - pulse_apparition_time[i - 1], 4),)
-                if i != len(pulse_apparition_time) - 1
-                else self.final_time_phase + (1,)
+                else self.final_time_phase + (pulse_apparition_time[i] - pulse_apparition_time[i - 1],)
             )
 
         self.n_stim = len(self.final_time_phase)
@@ -148,9 +146,19 @@ class FunctionalElectricStimulationOptimalControlProgramIdentification(OptimalCo
         stimulation_interval_average = np.mean(self.final_time_phase)
         self.n_shooting = []
         for i in range(len(pulse_apparition_time)):
-            self.n_shooting.append(
-                stimulated_n_shooting if self.final_time_phase[i] < stimulation_interval_average else rest_n_shooting
-            )
+            if self.final_time_phase[i] > stimulation_interval_average:
+                temp_final_time = self.final_time_phase[i]
+                rest_n_shooting = int(stimulated_n_shooting * temp_final_time / stimulation_interval_average)
+                self.n_shooting.append(rest_n_shooting)
+            else:
+                self.n_shooting.append(stimulated_n_shooting)
+
+        self.force_at_node = []
+        for i in range(self.n_stim):
+            for j in range(self.n_shooting[i]):
+                temp_time = sum(self.final_time_phase[:i]) + j * self.final_time_phase[i] / self.n_shooting[i]
+                temp_interpolated_force = np.interp(temp_time, force_tracking[0], force_tracking[1])
+                self.force_at_node.append(temp_interpolated_force)
 
         self.constraints = ConstraintList()
         self._set_parameters()
@@ -309,36 +317,37 @@ class FunctionalElectricStimulationOptimalControlProgramIdentification(OptimalCo
                     raise ValueError("All elements in objective kwarg must be an Objective type")
 
         if self.force_tracking:
+            node_idx = 0
             if self.with_fatigue:
-                for phase in range(self.n_stim):
-                    for i in range(self.n_shooting[phase]):
+                for i in range(self.n_stim):
+                    for j in range(self.n_shooting[i]):
                         self.objective_functions.add(
                             CustomObjective.track_state_from_time_interpolate,
                             custom_type=ObjectiveFcn.Mayer,
-                            node=i,
-                            time=self.force_tracking[0],
-                            force=self.force_tracking[1],
+                            node=j,
+                            force=self.force_at_node[node_idx],
                             key="F",
                             minimization_type="BF",
                             quadratic=True,
                             weight=1,
-                            phase=phase,
+                            phase=i,
                         )
+                        node_idx += 1
             else:
-                for phase in range(self.n_stim):
-                    for i in range(self.n_shooting[phase]):
+                for i in range(self.n_stim):
+                    for j in range(self.n_shooting[i]):
                         self.objective_functions.add(
                             CustomObjective.track_state_from_time_interpolate,
                             custom_type=ObjectiveFcn.Mayer,
-                            node=i,
-                            time=self.force_tracking[0],
-                            force=self.force_tracking[1],
+                            node=j,
+                            force=self.force_at_node[node_idx],
                             key="F",
                             minimization_type="LS",
                             quadratic=True,
                             weight=1,
-                            phase=phase,
+                            phase=i,
                         )
+                        node_idx += 1
 
     def _set_parameters(self):
         self.parameters = ParameterList()

identification/parameter_identification.py
@@ -45,8 +45,18 @@ class DingModelFrequencyParameterIdentification:
                         f"In the given list, all f_muscle_force_model_data_path must be str type,"
                         f" path index n°{i} is not str type"
                     )
+                if not force_model_data_path[i].endswith(".pkl"):
+                    raise TypeError(
+                        f"In the given list, all f_muscle_force_model_data_path must be pickle type and end with .pkl,"
+                        f" path index n°{i} is not ending with .pkl"
+                    )
         elif isinstance(force_model_data_path, str):
             force_model_data_path = [force_model_data_path]
+            if not force_model_data_path[0].endswith(".pkl"):
+                raise TypeError(
+                    f"In the given list, all f_muscle_force_model_data_path must be pickle type and end with .pkl,"
+                    f" path index is not ending with .pkl"
+                )
         else:
             raise TypeError(
                 f"In the given path, all f_muscle_force_model_data_path must be str type,"
@@ -61,8 +71,18 @@ class DingModelFrequencyParameterIdentification:
                         f"In the given list, all f_muscle_fatigue_model_data_path must be str type,"
                         f" path index n°{i} is not str type"
                     )
+                if not fatigue_model_data_path[i].endswith(".pkl"):
+                    raise TypeError(
+                        f"In the given list, all f_muscle_force_model_data_path must be pickle type and end with .pkl,"
+                        f" path index n°{i} is not ending with .pkl"
+                    )
         elif isinstance(fatigue_model_data_path, str):
             fatigue_model_data_path = [fatigue_model_data_path]
+            if not fatigue_model_data_path[0].endswith(".pkl"):
+                raise TypeError(
+                    f"In the given list, all f_muscle_force_model_data_path must be pickle type and end with .pkl,"
+                    f" path index is not ending with .pkl"
+                )
         else:
             raise TypeError(
                 f"In the given path, all f_muscle_fatigue_model_data_path must be str type,"
@@ -82,12 +102,6 @@ class DingModelFrequencyParameterIdentification:
             with open(force_model_data_path[i], "rb") as f:
                 data = pickle.load(f)
             force_model_data = data["biceps"]
-            # Arranging the data to have the beginning time starting at 0 second for all data
-            force_model_stim_apparition_time = (
-                data["stim_time"]
-                if data["stim_time"][0] == 0
-                else [stim_time - data["stim_time"][0] for stim_time in data["stim_time"]]
-            )
             force_model_time_data = (
                 data["time"]
                 if data["stim_time"][0] == 0
@@ -95,15 +109,19 @@ class DingModelFrequencyParameterIdentification:
             )
 
             # # Average on each force curve
-            # force_average = [x + y for x, y in zip(a, b)]
-            #
-            # # Estimated time
-            # force_model_time_data = np.linspace(0, len(force_average)/1000, len(force_average)/1000).tolist()
-            #
-            #
-            #
-            # # Estimated stimulation time
-            # force_model_stim_apparition_time = np.linspace(0, 1, 33).tolist()
+            smallest_list = 0
+            for j in range(len(force_model_data)):
+                if j == 0:
+                    smallest_list = len(force_model_data[j])
+                if len(force_model_data[j]) < smallest_list:
+                    smallest_list = len(force_model_data[j])
+            force_model_data = np.mean([row[:smallest_list] for row in force_model_data], axis=0).tolist()
+            force_model_time_data = [item for sublist in force_model_time_data for item in sublist]
+            force_model_time_data = force_model_time_data[:smallest_list]
+            frequency = 33
+            train_duration = 1
+            average_stim_apparition = np.linspace(0, train_duration, (frequency*train_duration)+1)
+            average_stim_apparition = np.append(average_stim_apparition, force_model_time_data[-1]).tolist()
 
             # Indexing the current data time on the previous one to ensure time continuity
             if i != 0:
@@ -112,19 +130,14 @@ class DingModelFrequencyParameterIdentification:
                     if discontinuity_phase_list == []
                     else discontinuity_phase_list[-1] + len(global_force_model_stim_apparition_time[-1]) - 1
                 )
-                force_model_stim_apparition_time = [
-                    stim_time + global_force_model_time_data[i - 1][-1]
-                    for stim_time in force_model_stim_apparition_time
-                ]
-                force_model_time_data = [
-                    [(time + global_force_model_time_data[i - 1][-1]) for time in row] for row in force_model_time_data
-                ]
-            # Expending lists
-            force_model_data = [item for sublist in force_model_data for item in sublist]
-            force_model_time_data = [item for sublist in force_model_time_data for item in sublist]
+
+                force_model_time_data = [(time + global_force_model_time_data[i - 1][-1]) for time in force_model_time_data]
+                average_stim_apparition = [(time + global_force_model_time_data[i - 1][-1]) for time in average_stim_apparition]
+                average_stim_apparition = average_stim_apparition[1:]
             # Storing data into global lists
             global_force_model_muscle_data.append(force_model_data)
-            global_force_model_stim_apparition_time.append(force_model_stim_apparition_time)
+            # global_force_model_stim_apparition_time.append(force_model_stim_apparition_time)
+            global_force_model_stim_apparition_time.append(average_stim_apparition)
             global_force_model_time_data.append(force_model_time_data)
         # Expending global lists
         global_force_model_muscle_data = [item for sublist in global_force_model_muscle_data for item in sublist]
@@ -132,11 +145,6 @@ class DingModelFrequencyParameterIdentification:
             item for sublist in global_force_model_stim_apparition_time for item in sublist
         ]
         global_force_model_time_data = [item for sublist in global_force_model_time_data for item in sublist]
-
-        # test slicing
-        # global_force_model_muscle_data = global_force_model_muscle_data[:30000]
-        # global_force_model_stim_apparition_time = global_force_model_stim_apparition_time[:50]
-        # global_force_model_time_data = global_force_model_time_data[:30000]
 
         # --- Fatigue model --- #
         global_fatigue_model_muscle_data = []
@@ -188,11 +196,6 @@ class DingModelFrequencyParameterIdentification:
         ]
         global_fatigue_model_time_data = [item for sublist in global_fatigue_model_time_data for item in sublist]
 
-        # test slicing
-        # global_fatigue_model_muscle_data = global_fatigue_model_muscle_data[:30000]
-        # global_fatigue_model_stim_apparition_time = global_fatigue_model_stim_apparition_time[:50]
-        # global_fatigue_model_time_data = global_fatigue_model_time_data[:30000]
-
         # --- Building force ocp --- #
         self.force_ocp = FunctionalElectricStimulationOptimalControlProgramIdentification(
             ding_model=model,
@@ -210,59 +213,59 @@ class DingModelFrequencyParameterIdentification:
 
         result = self.force_ocp.solve(Solver.IPOPT())
         # --- Print force model results --- #
-        # print(result.parameters)
-        # result_merged = result.merge_phases()
-        # plt.plot(result_merged.time, result_merged.states["F"][0], label="identification")
-        # global_force_model_muscle_data = np.array(
-        #     np.where(np.array(global_force_model_muscle_data) < 0, 0, global_force_model_muscle_data)
-        # ).tolist()
-        # plt.plot(global_force_model_time_data, global_force_model_muscle_data, label="tracking")
-        # plt.legend()
-        # plt.show()
+        print(result.parameters)
+        result_merged = result.merge_phases()
+        plt.plot(result_merged.time, result_merged.states["F"][0], label="identification")
+        global_force_model_muscle_data = np.array(
+            np.where(np.array(global_force_model_muscle_data) < 0, 0, global_force_model_muscle_data)
+        ).tolist()
+        plt.plot(global_force_model_time_data, global_force_model_muscle_data, label="tracking")
+        plt.legend()
+        plt.show()
 
         # --- Building fatigue ocp --- #
-        self.fatigue_ocp = FunctionalElectricStimulationOptimalControlProgramIdentification(
-            ding_model=model,
-            with_fatigue=True,
-            stimulated_n_shooting=5,  # 5
-            rest_n_shooting=165,  # 165
-            force_tracking=[np.array(global_fatigue_model_time_data), np.array(global_fatigue_model_muscle_data)],
-            pulse_apparition_time=global_fatigue_model_stim_apparition_time,
-            pulse_duration=None,
-            pulse_intensity=None,
+        # self.fatigue_ocp = FunctionalElectricStimulationOptimalControlProgramIdentification(
+        #     ding_model=model,
+        #     with_fatigue=True,
+        #     stimulated_n_shooting=5,  # 5
+        #     rest_n_shooting=165,  # 165
+        #     force_tracking=[np.array(global_fatigue_model_time_data), np.array(global_fatigue_model_muscle_data)],
+        #     pulse_apparition_time=global_fatigue_model_stim_apparition_time,
+        #     pulse_duration=None,
+        #     pulse_intensity=None,
             # a_rest=result.parameters["a_rest"],
             # km_rest=result.parameters["km_rest"],
             # tau1_rest=result.parameters["tau1_rest"],
             # tau2=result.parameters["tau2"],
-            a_rest=1582.43,
-            km_rest=1,
-            tau1_rest=0.0034,
-            tau2=0.2032,
-            discontinuity_in_ocp=discontinuity_phase_list,
-            use_sx=kwargs["use_sx"] if "use_sx" in kwargs else False,
-        )
-
-    # del global_fatigue_model_muscle_data, global_fatigue_model_stim_apparition_time, global_fatigue_model_time_data
-
-        result_fatigue = self.fatigue_ocp.solve(Solver.IPOPT())
-        # --- Print fatigue model results --- #
-        print(result_fatigue.parameters)
-        result_merged = result.merge_phases()
-        plt.plot(result_merged.time, result_merged.states["F"][0], label="identification")
-        global_fatigue_model_muscle_data = np.array(
-            np.where(np.array(global_fatigue_model_muscle_data) < 0, 0, global_fatigue_model_muscle_data)
-        ).tolist()
-        plt.plot(global_fatigue_model_time_data, global_fatigue_model_muscle_data, label="tracking")
-        plt.legend()
-        plt.show()
+    #         a_rest=1582.43,
+    #         km_rest=1,
+    #         tau1_rest=0.0034,
+    #         tau2=0.2032,
+    #         discontinuity_in_ocp=discontinuity_phase_list,
+    #         use_sx=kwargs["use_sx"] if "use_sx" in kwargs else False,
+    #     )
+    #
+    # # del global_fatigue_model_muscle_data, global_fatigue_model_stim_apparition_time, global_fatigue_model_time_data
+    #
+    #     result_fatigue = self.fatigue_ocp.solve(Solver.IPOPT())
+    #     # --- Print fatigue model results --- #
+    #     print(result_fatigue.parameters)
+    #     result_merged = result.merge_phases()
+    #     plt.plot(result_merged.time, result_merged.states["F"][0], label="identification")
+    #     global_fatigue_model_muscle_data = np.array(
+    #         np.where(np.array(global_fatigue_model_muscle_data) < 0, 0, global_fatigue_model_muscle_data)
+    #     ).tolist()
+    #     plt.plot(global_fatigue_model_time_data, global_fatigue_model_muscle_data, label="tracking")
+    #     plt.legend()
+    #     plt.show()
 
 
 if __name__ == "__main__":
     DingModelFrequencyParameterIdentification(
         model=DingModelFrequency,
-        force_model_data_path=["D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_0.pkl"],
-                               # "D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_0.pkl",
-                               # "D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_0.pkl"],
+        force_model_data_path=["D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_0.pkl",
+                               "D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_0.pkl",
+                               "D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_0.pkl"],
         fatigue_model_data_path=[
             "D:/These/Programmation/Modele_Musculaire/optistim/data_process/biceps_force_fatigue_0.pkl"
         ],

optistim/custom_objectives.py
@@ -40,8 +40,8 @@ class CustomObjective:
 
     @staticmethod
     def track_state_from_time_interpolate(
-        controller: PenaltyController, time: np.ndarray, force: np.ndarray, key: str, minimization_type: str = "LS"
-    ) -> MX | SX:
+        controller: PenaltyController, force: np.ndarray, key: str, minimization_type: str = "LS"
+    ) -> MX:
         """
         Minimize the states variables.
         This function least square.
@@ -51,8 +51,6 @@ class CustomObjective:
         ----------
         controller: PenaltyController
             The penalty node elements
-        time: np.ndarray
-            The force data time vector
         force: np.ndarray
             The force vector
         key: str
@@ -65,18 +63,8 @@ class CustomObjective:
         The difference between the two keys
         """
         if minimization_type == "LS":
-            interpolated_force = np.interp(
-                controller.ocp.node_time(phase_idx=controller.phase_idx, node_idx=controller.t[0]), time, force
-            )
-            interpolated_force = 0 if interpolated_force < 0 else interpolated_force
-            return interpolated_force - controller.states[key].cx
-        elif minimization_type == "BF":  # TODO : remove numpy
-            interpolated_force = np.interp(
-                controller.ocp.node_time(phase_idx=controller.phase_idx, node_idx=controller.t[0]), time, force
-            )
-            if interpolated_force < 0:
-                return SX(0) if controller.cx.type_name() == "SX" else MX(0)
-            else:
-                return 1 - (interpolated_force / controller.states[key].cx)
+            return force - controller.states[key].cx
+        elif minimization_type == "BF":
+            return 1 - (force / controller.states[key].cx)
         else:
             raise RuntimeError(f"Minimization type {minimization_type} not implemented")