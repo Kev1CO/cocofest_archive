b65aa9d66ae486326ad0bb0a6a70a6eb4bcd4fa4
Kevin CO <kevinco@umontreal.ca>
2023-11-23 16:16:24 -0500

    again

--- external/bioptim_install_windows.sh
+++ external/bioptim_install_windows.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Move to the external folder
-cd ${0%/*}
-
-# Move to the bioptim folder
-cd bioptim
+## Move to the external folder
+#cd ${0%/*}
+#
+## Move to the bioptim folder
+#cd bioptim
 
 # Installing bioptim
 python setup.py install
